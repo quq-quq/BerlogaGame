3a6109c5652dbe6809f1b72aaaed0ce8dd389f57
quq-quq <kubrakfedor@gmail.com>
2024-12-17 21:15:34 +0300

    UIChanges

--- Assets/Core/Gameplay/Other/LevelButtonsScript.cs
+++ Assets/Core/Gameplay/Other/LevelButtonsScript.cs
@@ -11,14 +11,23 @@ public class LevelButtonsScript : MonoBehaviour
     [SerializeField] private GameObject _pausePanel;
     [SerializeField] private AudioClip _backgroundSound;
     [SerializeField] private float _volume;
-    [SerializeField] private Image _muteButton;
-    [SerializeField] private Sprite _mute;
-    [SerializeField] private Sprite _unmute;
+    [SerializeField] private Transform _parentOfButtons;
+    [SerializeField] private Image _manualButtonImage;
+    [SerializeField] private Sprite _manualActiveSprite;
+    [SerializeField] private Sprite _manualInactiveSprite;
+    [SerializeField] private Image _muteButtonImage;
+    [SerializeField] private Sprite _muteSprite;
+    [SerializeField] private Sprite _unmuteSprite;
+    [SerializeField] private Image _pauseButtonImage;
+    [SerializeField] private Sprite _pausedSprite;
+    [SerializeField] private Sprite _unpausedSprite;
     [SerializeField] private GameObject _manual;
     [SerializeField] private SceneData _menuScene;
     [SerializeField] private Button _suggestionButton;
     [SerializeField, Min(0f)] private float _suggestionCooldown = 5f;
     [SerializeField] private CopilotMonolog _copilotMonolog;
+    [SerializeField] private GameObject _nodeDesk, _buttonDesk;
+    [SerializeField] private StateOfPlayerController _stateOfPlayerController;
     private bool _isPaused, _isMusicOff, _suggestionIsActive = true, _isCoolDownAccepted = false;
     private SceneLoader _sceneLoader;
     private Image _suggestionImage;
@@ -35,11 +44,11 @@ public class LevelButtonsScript : MonoBehaviour
         AudioListener.pause = Saver.Data.IsMute;
         if(Saver.Data.IsMute)
         {
-            _muteButton.sprite = _mute;
+            _muteButtonImage.sprite = _muteSprite;
         }
         else
         {
-            _muteButton.sprite = _unmute;
+            _muteButtonImage.sprite = _unmuteSprite;
         }
         _isMusicOff = Saver.Data.IsMute;
         _pausePanel.SetActive(false);
@@ -61,16 +70,21 @@ public class LevelButtonsScript : MonoBehaviour
     {
         if (!_isPaused)
         {
-            Time.timeScale = 0;
-            _pausePanel.SetActive(true);
+            _pauseButtonImage.sprite = _pausedSprite;
             _isPaused = true;
+            _pausePanel.SetActive(true);
+            SetOnlyCurrentButton(_pauseButtonImage.gameObject);
+            Time.timeScale = 0;
         }
         else
         {
             Time.timeScale = 1f;
             _pausePanel.SetActive(false);
             _isPaused = false;
+            _pauseButtonImage.sprite = _unpausedSprite;
+            SetAllButtonsOn();
         }
+        _stateOfPlayerController.SetStateOfComponent();
     }
 
     public void StopMusic()
@@ -79,14 +93,14 @@ public class LevelButtonsScript : MonoBehaviour
         {
             AudioListener.pause = true;
             _isMusicOff = true;
-            _muteButton.sprite = _mute;
+            _muteButtonImage.sprite = _muteSprite;
             Saver.Data.IsMute = true;
         }
         else
         {
             AudioListener.pause = false;
             _isMusicOff = false;
-            _muteButton.sprite = _unmute;
+            _muteButtonImage.sprite = _unmuteSprite;
             Saver.Data.IsMute = false;
         }
         Saver.Save();
@@ -95,6 +109,17 @@ public class LevelButtonsScript : MonoBehaviour
     public void OpenCloseManual()
     {
        _manual.SetActive(!_manual.activeSelf);
+        if (_manual.activeSelf)
+        {
+            SetOnlyCurrentButton(_manualButtonImage.gameObject);
+            _manualButtonImage.sprite = _manualActiveSprite;
+        }
+        else
+        {
+            SetAllButtonsOn();
+            _manualButtonImage.sprite = _manualInactiveSprite;
+        }
+        _stateOfPlayerController.SetStateOfComponent();
     }
 
     public void RestartScene()
@@ -104,6 +129,7 @@ public class LevelButtonsScript : MonoBehaviour
         _isPaused = false;
         _sceneLoader.LoadScene(_sceneLoader.GetCurrentScene());
     }
+
     public void Suggestion()
     {
          _copilotMonolog.TakeSuggestion();
@@ -131,5 +157,58 @@ public class LevelButtonsScript : MonoBehaviour
             _suggestionIsActive = true;
             _suggestionButton.interactable = true;
         }
+    }
+
+    public void SetOnlyCurrentButton(GameObject currentButton)
+    {
+        bool on = false;
+
+        foreach (Transform button in _parentOfButtons)
+        {
+            if(button.gameObject == currentButton)
+            {
+                continue;
+            }
+            if (button.gameObject.activeSelf)
+            {
+                on = true;
+                break;
+            }
+        }
+
+        if (on)
+        {
+            foreach (Transform button in _parentOfButtons)
+            {
+                if (button.gameObject != currentButton)
+                {
+                    button.gameObject.SetActive(false);
+                }
+            }
+            if(currentButton != _buttonDesk)
+                _buttonDesk.SetActive(false);
+        }
+        else
+        {
+            SetAllButtonsOn();
+        }
+    }
+
+    public void SetAllButtonsOn()
+    {
+        foreach (Transform button in _parentOfButtons)
+        {
+            button.gameObject.SetActive(true);   
+        }
+        _buttonDesk.SetActive(true);
+    }
+
+    public void SetAllButtonsOff()
+    {
+        foreach (Transform button in _parentOfButtons)
+        {
+            button.gameObject.SetActive(false);
+        }
+        _buttonDesk.SetActive(false);
     }
 }
---
+++ Assets/Core/Gameplay/Player/StateOfPlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class StateOfPlayerController : MonoBehaviour
+{
+    private bool _isEnabled = true;
+    private PlayerController _playerController;
+
+    private void Start()
+    {
+        _playerController = GetComponent<PlayerController>();
+    }
+
+    public void SetStateOfComponent()
+    {
+        if(_isEnabled)
+        {
+            _playerController.enabled = false;
+            _isEnabled = false;
+        }
+        else
+        {
+            _playerController.enabled = true;
+            _isEnabled = true;
+        }
+    } 
+}
