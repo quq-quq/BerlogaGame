0693454a2d1b71e0962412eeed41386f34d6e7d2
quq-quq <kubrakfedor@gmail.com>
2024-12-25 01:23:36 +0300

    4thSceneDone

--- Assets/Core/Gameplay/Other/StarController.cs
+++ Assets/Core/Gameplay/Other/StarController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,10 +6,20 @@ using UnityEngine;
 
 public class StarController : MonoBehaviour
 {
+
     [SerializeField] private DialogueTrigger _DialogueRobot;
     [SerializeField] private AudioClip _audioClip;
     [Space]
+    [SerializeField, Min(0)] private float _distanceUp;
+    [SerializeField] private float _rotation;
+    [SerializeField, Min(0)] private float _durationOfMove;
     [SerializeField, Range(0, 1)] private float _volume = 0.5f;
+
+    private void Start()
+    {
+        transform.DOMoveY(transform.position.y+_distanceUp, _durationOfMove).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+        transform.DORotate(new Vector3(0, 0, _rotation) , _durationOfMove).SetEase(Ease.InOutElastic).SetLoops(-1, LoopType.Yoyo);
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,5 +30,10 @@ public class StarController : MonoBehaviour
 
         SoundController.sounder.SetSound(_audioClip, false, gameObject.name, _volume);
         Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        transform.DOKill();
     }
 }
